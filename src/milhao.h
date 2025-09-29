1369b3387044e290571898dae9a57765c7264beb
caio-o <caiusbarbieri@gmail.com>
2025-09-29 19:07:33 -0400

    Adicionar tipo <pergunta>

--- src/milhao.h
+++ src/milhao.h
@@ -7,9 +7,31 @@
  */
 
 /**
+ *  Pergunta que pode ser feita ao jogador.
+ *
+ *  nivel:        dificuldade da questão. 
+ *  descricicao:  enunciado de tamanho maximo 200 ( termina em '\0' ).
+ *  alt:          vetor de strings correspondedo as possiveis respostas.
+ *  alt_correta:  alternativa correta dentre as <alt>
+ */
+typedef struct
+{
+	char nivel;
+	char descricao[200];
+	char alt[4][30];
+	char alt_correta;
+} pergunta;
+
+/**
  *  Espera que o usuário termine a linha antes de continuar.
  *
  */
 void esperaEnter ();
+
+/**
+ *  Seleciona uma pergunta aleatória do nível dado.
+ *
+ */
+pergunta* pegaPergunta (char nivel);
 
 #endif
